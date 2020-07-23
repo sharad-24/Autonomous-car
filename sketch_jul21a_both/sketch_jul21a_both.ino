99823647d7848f3cb53e264d73e40291ddce80d4
sharad-24 <500075287@stu.upes.ac.in>
2020-07-23 22:02:37 +0530

    Code

---
+++ sketch_jul21a_both/sketch_jul21a_both.ino
@@ -0,0 +1,165 @@
+#include <AFMotor.h>
+
+#include<NewPing.h>
+
+//defining pins and variables
+#define left A1
+#define right A0
+#define TRIG A2 
+#define ECHO A3 
+#define MAX_DISTANCE 100 
+NewPing sonar(TRIG, ECHO, MAX_DISTANCE); 
+
+//defining motors
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+
+
+
+
+void setup() {
+
+  //declaring pin types
+  pinMode(left,INPUT);
+  pinMode(right,INPUT);
+  //begin serial communication
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+
+  delay(70);
+int distance = sonar.ping_cm();
+if (distance == 0) {
+  distance = 30;
+}
+if(distance <=15) {
+  Stop();
+  delay(100);
+leftmove();
+delay(550);
+forward();
+delay(1000);
+rightmove();  
+delay(200);
+forward();
+delay(500);
+}
+
+
+  //line detected by none
+   if(analogRead(left)<=400 && analogRead(right)<=400){
+    //Forward
+    motor1.run(FORWARD);
+    motor1.setSpeed(60);
+    motor2.run(FORWARD);
+    motor2.setSpeed(60);
+    motor3.run(FORWARD);
+    motor3.setSpeed(60);
+    motor4.run(FORWARD);
+    motor4.setSpeed(60);
+  }
+
+
+  //line detected by left sensor
+  else if(analogRead(left)<=400 && !analogRead(right)<=400){
+    //turn left
+    motor1.run(FORWARD);
+    motor1.setSpeed(200);
+    motor2.run(FORWARD);
+    motor2.setSpeed(200);
+    motor3.run(BACKWARD);
+    motor3.setSpeed(200);
+    motor4.run(BACKWARD);
+    motor4.setSpeed(200);
+    
+  }
+  //line detected by right sensor
+  else if(!analogRead(left)<=400 && analogRead(right)<=400){
+    //turn right
+    motor1.run(BACKWARD);
+    motor1.setSpeed(200);
+    motor2.run(BACKWARD);
+    motor2.setSpeed(200);
+    motor3.run(FORWARD);
+    motor3.setSpeed(200);
+    motor4.run(FORWARD);
+    motor4.setSpeed(200);
+   
+  }
+  //line detected by both
+  else if(!analogRead(left)<=400 && !analogRead(right)<=400){
+    //stop
+    motor1.run(RELEASE);
+    motor1.setSpeed(0);
+    motor2.run(RELEASE);
+    motor2.setSpeed(0);
+    motor3.run(RELEASE);
+    motor3.setSpeed(0);
+    motor4.run(RELEASE);
+    motor4.setSpeed(0);
+   
+  }
+
+  
+}
+void forward()
+{
+     motor1.run(FORWARD);
+     motor1.setSpeed(200);
+  motor2.run(FORWARD);
+  motor2.setSpeed(200);
+  motor3.run(FORWARD);
+  motor3.setSpeed(200);
+  motor4.run(FORWARD);
+  motor4.setSpeed(200);
+}
+
+void backward()
+{
+     motor1.run(BACKWARD);
+     motor1.setSpeed(200);
+  motor2.run(BACKWARD);
+  motor2.setSpeed(200);
+  motor3.run(BACKWARD);
+  motor3.setSpeed(200);
+  motor4.run(BACKWARD);
+  motor4.setSpeed(200);
+}
+void leftmove()
+{
+  motor1.run(FORWARD);
+  motor1.setSpeed(200);
+  motor2.run(FORWARD);
+  motor2.setSpeed(200);
+  motor3.run(BACKWARD);
+  motor3.setSpeed(200);
+  motor4.run(BACKWARD);
+  motor4.setSpeed(200);
+}
+void rightmove()
+{
+  motor1.run(BACKWARD);
+  motor1.setSpeed(200);
+  motor2.run(BACKWARD);
+  motor2.setSpeed(200);
+  motor3.run(FORWARD);
+  motor3.setSpeed(200);
+  motor4.run(FORWARD);
+  motor4.setSpeed(200);
+}
+void Stop()
+{
+  motor1.run(RELEASE);
+  motor1.setSpeed(0);
+  motor2.run(RELEASE);
+  motor2.setSpeed(0);
+  motor3.run(RELEASE);
+  motor3.setSpeed(0);
+  motor4.run(RELEASE);
+  motor4.setSpeed(0);
+}
